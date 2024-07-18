e45dc91534fdc803ac9441777fd327d514fc0a12
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-18 19:02:55 +0530

    Container With Most Water

---
+++ ARRAY/13ContainerWithMostWater.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxArea(vector<int> &height){
+    int n = height.size();
+    int i = 0, j = n - 1;
+    int maxWater = 0;
+    while (i < j){
+        int width=j-i;
+        int currheight=min(height[i],height[j]);
+        int area=width*currheight;
+        maxWater=max(maxWater,area);
+        if(height[i]<height[j]){
+            i++;
+        }else j--;
+    }
+    return maxWater;
+}
+
+int main()
+{
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    int ans = maxArea(height);
+    cout << "Container With Most Water: " << ans << endl;
+    return 0;
+}
