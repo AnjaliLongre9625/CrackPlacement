446d51791f96bc5307ac8a46508b2106e0103777
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-18 23:03:42 +0530

    Print all the duplicate characters in a string

--- ARRAY/16AllduplicateinString.cpp
+++ ARRAY/16AllduplicateinString.cpp
@@ -1,21 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void printDup(string str){
-   int n=str.size();
-   unordered_map<char ,int>count;
-   for(int i=0;i<n;i++){
-     count[str[i]]++;
-   }
-   for(auto it:count){
-    if(it.second>1){
-        cout << it.first << ", count = " << it.second<< "\n";
+    int n = str.size();
+    unordered_map<char, int> count;
+    for (int i = 0; i < n; i++){
+        count[str[i]]++;
     }
-  }
+    for (auto it : count){
+        if (it.second > 1){
+            cout << it.first << ", count = " << it.second << "\n";
+        }
+    }
 }
-
 int main(){
-    string strs="abbcadeeefa";
+    string strs = "abbcadeeefa";
     printDup(strs);
     return 0;
 }
