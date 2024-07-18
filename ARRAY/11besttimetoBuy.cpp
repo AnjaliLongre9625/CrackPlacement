af46f8d5bc1509040139c5a6fc015e6ef2f3e033
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-18 16:07:15 +0530

    Best Time to Buy and Sell Stock

---
+++ ARRAY/11besttimetoBuy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int maxProfit(vector<int>& prices) {
+        int minSoFar=prices[0];
+        int maxProfit=0;
+
+        for(int i=1;i<prices.size();i++){
+            minSoFar=min(minSoFar,prices[i]);
+            int profit=prices[i]-minSoFar;
+            maxProfit=max(maxProfit,profit);
+           }
+        return maxProfit;
+    }
+int main(){
+    vector<int> prices = {7,1,5,3,6,4};
+    int ans = maxProfit(prices);
+    cout << "The max profit is: " << ans << endl;
+    return 0;
+}
