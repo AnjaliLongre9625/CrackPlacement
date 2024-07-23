cf2a8a0767ccdf49957148627fb6f446b7ec3573
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-23 19:15:53 +0530

    Add Binary

--- MATHEMATICAL/AddBinary.cpp
+++ MATHEMATICAL/AddBinary.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string addBinary(string a, string b){
+string addBinary(string a, string b)
+{
     int alen = a.length();
     int blen = b.length();
     int carry = 0, i = 0;
     string ans = "";
 
-    while (i<alen || i<blen || carry!=0)
+    while (i < alen || i < blen || carry != 0)
     {
         int x = 0;
         if (i < alen && a[alen - i - 1] == '1')
