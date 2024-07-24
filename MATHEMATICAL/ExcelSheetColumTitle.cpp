c9c34fcda225117233cefeb159ea5c9ae1bd9ea0
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-24 12:40:09 +0530

    Product array puzzle && productExceptSelf

---
+++ MATHEMATICAL/ExcelSheetColumTitle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+string convertToTitle(int columnNumber) {
+        
+}
+
+int main()
+{
+    int columnNumber1=1;
+    cout << "Minimum moves to equalize elements: " << convertToTitle(columnNumber1) << endl;
+    return 0;
+}
