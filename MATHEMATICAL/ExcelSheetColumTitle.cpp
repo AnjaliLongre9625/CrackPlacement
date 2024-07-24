75d32fe82b9344136e5b043b79a8fbd78e6304b1
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-07-24 13:06:39 +0530

    Excel Sheet Column Title

--- MATHEMATICAL/ExcelSheetColumTitle.cpp
+++ MATHEMATICAL/ExcelSheetColumTitle.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-string convertToTitle(int columnNumber) {
-        
+string convertToTitle(int n)
+{
+    string result = "";
+
+    while (n)
+    {
+        char c = 'A' + (n - 1) % 26;
+        result = c + result;
+        n = (n - 1) / 26;
+    }
+    return result;
 }
 
 int main()
 {
-    int columnNumber1=1;
-    cout << "Minimum moves to equalize elements: " << convertToTitle(columnNumber1) << endl;
+    int columnNumber1 = 28;
+    cout << "Excel Sheet Column Title: " << convertToTitle(columnNumber1) << endl;
     return 0;
 }
