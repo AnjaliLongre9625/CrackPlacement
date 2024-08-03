fa8f357ed4d14b14f2fa26368f7ab5051bfe34e5
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-08-03 11:48:46 +0530

    Permutations in array

---
+++ SORTING&SEARCHING/Permutationsinarray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end(), greater<int>());
+        
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1[i] + arr2[i] < k) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    // Test case 1
+    int k1 = 10;
+    vector<int> arr1_1 = {2, 1, 3};
+    vector<int> arr2_1 = {7, 8, 9};
+    
+    bool result1 = solution.isPossible(k1, arr1_1, arr2_1);
+    cout << "Test Case 1: " << (result1 ? "True" : "False") << endl;
+    
+    return 0;
+}
