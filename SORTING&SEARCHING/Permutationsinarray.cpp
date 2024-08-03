1746983c4e5b81997369e63fec79c3a87dffa9ad
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-08-03 11:49:58 +0530

    Permutations in array

--- SORTING&SEARCHING/Permutationsinarray.cpp
+++ SORTING&SEARCHING/Permutationsinarray.cpp
@@ -9,8 +9,8 @@ public:
     bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
         sort(arr1.begin(), arr1.end());
         sort(arr2.begin(), arr2.end(), greater<int>());
-        
-        for (int i = 0; i < arr1.size(); i++) {
+        int n=arr1.size();
+        for (int i = 0; i <n; i++) {
             if (arr1[i] + arr2[i] < k) {
                 return false;
             }
