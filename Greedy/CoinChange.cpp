4a9c2da86a002ce4634adec9bc87af358aff3e7e
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-10-04 20:53:20 +0530

    Coin Change – Minimum Coins to Make Sum

---
+++ Greedy/CoinChange.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minCoins(int coins[], int m, int sum)
+{
+    if(sum==0) return 0;
+    int cnt=0;
+    for(int i=m-1;i>=0;i--){
+        while(sum>coins[i]){
+             sum-=coins[i];
+             cnt+=1;
+        }
+    }
+    return cnt;
+}
+
+// Driver program to test above function
+int main()
+{
+    int coins[] =  {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    int m = sizeof(coins)/sizeof(coins[0]);
+    int sum = 49;
+    cout << "Minimum coins required is "
+         << minCoins(coins, m, sum);
+    return 0;
+}
