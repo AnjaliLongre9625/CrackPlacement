a2186efb4244a48f648313f16efce13959812cf0
AnjaliLongre9625 <anjalilongre349@gmail.com>
2024-11-02 01:40:20 +0530

    Binary Tree Inorder Traversal

---
+++ TREE/BINARY TREE/iterativeInorderBT.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+
+        while (true) {
+            if (node != NULL) {
+                st.push(node);
+                node = node->left;
+            } else {
+                if (st.empty()) break;
+                node = st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return inorder;
+    }
+};
+
+// Helper function to create a sample binary tree for testing
+TreeNode* createSampleTree() {
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+    return root;
+}
+
+int main() {
+    Solution sol;
+    TreeNode* root = createSampleTree();
+    vector<int> result = sol.inorderTraversal(root);
+
+    cout << "Inorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
