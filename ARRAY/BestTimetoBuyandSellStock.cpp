e2970ec4d27e41d2fa9d4e2d517adee7cdf8caa0
Anjali Longre <anjalilongre349@gmail.com>
2025-05-30 13:52:53 +0530

    buy and sell

--- ARRAY/BestTimetoBuyandSellStock.cpp
+++ ARRAY/BestTimetoBuyandSellStock.cpp
@@ -14,6 +14,6 @@ using namespace std;
 int main(){
     vector<int> prices = {7,1,5,3,6,4};
     int ans = maxProfit(prices);
-    cout << "The max profit is: " << ans << endl;
+    cout << "The max profit after see=lling is: " << ans << endl;
     return 0;
 }
