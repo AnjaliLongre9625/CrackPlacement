b31c8e30df454da7f7d926ac50f5a2b20d48cb1f
Anjali Longre <anjalilongre349@gmail.com>
2025-05-30 11:04:09 +0530

    Buy and Sell

--- BLIND75/Array/BestTotimebuy.cpp
+++ BLIND75/Array/BestTotimebuy.cpp
@@ -4,15 +4,16 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int miniSoFar=prices[0];
+        int mini=prices[0];
         int maxi=0;
         for(int i=1;i<prices.size();i++){
-            miniSoFar=min(miniSoFar,prices[i]);
-            maxi=max(maxi,prices[i]-miniSoFar);
+            mini=min(mini,prices[i]);
+            maxi=max(maxi,prices[i]-mini);
         }
     return maxi;
   }
 };
+
 int main() {
     Solution solution;
     vector<int> prices = {7, 1, 5, 3, 6, 4};// Example input
