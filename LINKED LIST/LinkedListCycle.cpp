23321ebe4920a5fe996631642a45182b0ea7225d
Anjali Longre <anjalilongre349@gmail.com>
2025-06-03 11:50:07 +0530

    initial

--- LINKED LIST/LinkedListCycle.cpp
+++ LINKED LIST/LinkedListCycle.cpp
@@ -19,7 +19,7 @@ bool hasCycle(ListNode *head) {
             slow=slow->next;
             fast=fast->next->next;
              
-        if(slow==fast) return true;
+            if(slow==fast) return true;
         }
         return false;
     }
