e08f71299e10139109affe467c6b376da3cbf394
Anjali Longre <anjalilongre349@gmail.com>
2025-06-06 00:14:34 +0530

    initial

--- SORTING&SEARCHING/reversingarraysorted.cpp
+++ SORTING&SEARCHING/reversingarraysorted.cpp
@@ -12,12 +12,11 @@ bool sortArr(int a[], int n) {
             y = i + 1;
         }
     }
-    
+
     if (x != -1){
         reverse(a + x, a + y + 1);
         for (int i = 0; i < n - 1; i++){
-            if (a[i] > a[i + 1])
-            {
+            if (a[i] > a[i + 1]){
                 return false;
                 return 0;
             }
