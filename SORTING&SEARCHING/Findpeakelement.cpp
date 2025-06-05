801614d8c3d544f30fb31d2114cbf90adc191e37
Anjali Longre <anjalilongre349@gmail.com>
2025-06-05 14:46:28 +0530

    initial

--- SORTING&SEARCHING/Findpeakelement.cpp
+++ SORTING&SEARCHING/Findpeakelement.cpp
@@ -11,10 +11,11 @@ int findPeak(int arr[], int n) {
 
     while(low<high){
         int mid=(low+high)/2;
-        
+
         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
             return mid;
         }
+        
         else if(arr[mid]>arr[mid-1]){
             low=mid+1;
         }
