9fe97dd033ebd455b8c69d9815b41ba4a2707c05
Anjali Longre <anjalilongre349@gmail.com>
2025-06-06 16:40:02 +0530

    initial

--- GRAPH/G2GraphListRep.cpp
+++ GRAPH/G2GraphListRep.cpp
@@ -8,11 +8,11 @@ int main(){
     vector<int> adj[n+1];
 
     for(int i=0;i<m;i++){ 
+        
        int u,v;  
        cin>>u>>v;
        adj[u].push_back(v);
        adj[v].push_back(u);
-
     }
 
     return 0;
