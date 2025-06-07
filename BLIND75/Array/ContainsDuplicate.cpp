82d52a49572cdc4639856770196a53295c49cf95
Anjali Longre <anjalilongre349@gmail.com>
2025-06-07 20:03:46 +0530

    initial

--- BLIND75/Array/ContainsDuplicate.cpp
+++ BLIND75/Array/ContainsDuplicate.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-bool containsDuplicate(vector<int>& nums) {
-      unordered_set<int>uniqueSet;
-      for(int num:nums){
-          if(uniqueSet.count(num)>0){
-              return true;
-          }
-          uniqueSet.insert(num);
-       }
-      return false;
-     }
-   };
-int main() {
+    bool containsDuplicate(vector<int> &nums) {
+        unordered_set<int> uniqueSet;
+        for (int num : nums)
+        {
+            if (uniqueSet.count(num) > 0)
+            {
+                return true;
+            }
+            uniqueSet.insert(num);
+        }
+        return false;
+    }
+};
+int main()
+{
     Solution solution;
 
     vector<int> prices = {7, 1, 5, 3, 6, 4};
