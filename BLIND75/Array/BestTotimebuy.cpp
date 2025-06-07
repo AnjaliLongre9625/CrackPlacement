de38d92e06953944afbf95bc38e4ced204a1d6c5
Anjali Longre <anjalilongre349@gmail.com>
2025-06-07 20:02:43 +0530

    initial

--- BLIND75/Array/BestTotimebuy.cpp
+++ BLIND75/Array/BestTotimebuy.cpp
@@ -6,6 +6,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int mini=prices[0];
         int maxi=0;
+        
         for(int i=1;i<prices.size();i++){
             int cost=prices[i]-mini;
             mini=min(mini,prices[i]);
