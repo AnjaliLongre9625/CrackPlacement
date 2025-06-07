4327e47d7e0d780a270bfb132d5c9a7d683b9435
Anjali Longre <anjalilongre349@gmail.com>
2025-06-07 20:05:38 +0530

    initial

--- Binary/1CountingBits.cpp
+++ Binary/1CountingBits.cpp
@@ -2,24 +2,29 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> countBits(int n) {
-        vector<int>ans(n+1,0);
-        for(int i=1;i<=n;i++){
-           ans[i]=ans[i/2]+(i%2);
+    vector<int> countBits(int n)
+    {
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            ans[i] = ans[i / 2] + (i % 2);
         }
-        return ans ;
+        return ans;
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
-    int n = 5;  // Example input
+    int n = 5; // Example input
     vector<int> result = sol.countBits(n);
 
     cout << "The count of set bits from 0 to " << n << " is: ";
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
+    {
         cout << result[i] << " ";
     }
     cout << endl;
