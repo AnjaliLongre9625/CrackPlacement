a61fbc88dc7ecc018a6d21717eae9146728b78d4
Anjali Longre <anjalilongre349@gmail.com>
2025-06-07 20:04:15 +0530

    initial

--- BLIND75/Array/ContainsDuplicate.cpp
+++ BLIND75/Array/ContainsDuplicate.cpp
@@ -7,8 +7,7 @@ public:
     bool containsDuplicate(vector<int> &nums) {
         unordered_set<int> uniqueSet;
         for (int num : nums){
-            if (uniqueSet.count(num) > 0)
-            {
+            if (uniqueSet.count(num) > 0){
                 return true;
             }
             uniqueSet.insert(num);
