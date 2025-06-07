f2e532fb7bdfaf2fb3ae4685528a41680bc03a91
Anjali Longre <anjalilongre349@gmail.com>
2025-06-07 20:06:31 +0530

    initial

--- Binary/1CountingBits.cpp
+++ Binary/1CountingBits.cpp
@@ -2,11 +2,9 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution{
 public:
-    vector<int> countBits(int n)
-    {
+    vector<int> countBits(int n){
         vector<int> ans(n + 1, 0);
         for (int i = 1; i <= n; i++)
         {
