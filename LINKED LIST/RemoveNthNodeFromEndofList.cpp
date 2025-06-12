59335926d3253e31a90822423bb19ca54bfc16a3
Anjali Longre <anjalilongre349@gmail.com>
2025-06-12 18:51:07 +0530

    initial

--- LINKED LIST/RemoveNthNodeFromEndofList.cpp
+++ LINKED LIST/RemoveNthNodeFromEndofList.cpp
@@ -6,7 +6,6 @@ struct ListNode{
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
-
 ListNode *removeNthFromEnd(ListNode *head, int n){
     ListNode *fast = head;
     ListNode *slow = head;
