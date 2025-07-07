e4cbcc8360bff49f3eed35ef2432955f5999bf75
Anjali Longre <anjalilongre349@gmail.com>
2025-07-07 20:48:35 +0530

    updated

--- MATHEMATICAL/AddBinary.cpp
+++ MATHEMATICAL/AddBinary.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-string addBinary(string a, string b)
-{
+string addBinary(string a, string b){
     int alen = a.length();
     int blen = b.length();
     int carry = 0, i = 0;
     string ans = "";
-
 
     while (i < alen || i < blen || carry != 0){
         int x = 0;
